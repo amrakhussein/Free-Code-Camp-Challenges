de837ca709886ce1805102542a7f74f47c8a47df
Amro Abdelkamel <amromoorie@gmail.com>
2022-01-31 23:34:53 +0200

    prepare

---
+++ cash-register.js
@@ -0,0 +1,15 @@
+// {status: "INSUFFICIENT_FUNDS", change: []}
+// {status: "CLOSED", change: [...]}
+// {status: "OPEN", change: [...]}
+
+  
+  function checkCashRegister(price, cash, cid) {
+
+  
+  }
+  
+
+  const foo = checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 20]])
+  console.log('foo: ', foo);
+
+
